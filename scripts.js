f5ac2f6d52672a658c27ac1bb7e6045d007632cb
PavelKochetkov <106112045+PavelKochetkov@users.noreply.github.com>
2022-06-01 01:11:28 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,46 +1,54 @@
 // var name = document.querySelector('#name')
-var about = document.querySelector('#about')
-var skill = document.querySelector('#skill')
-var contacts = document.querySelector('#contacts')
+let about = document.querySelector('#about')
+let skill = document.querySelector('#skill')
+let contacts = document.querySelector('#contacts')
 
-// function setColor(e) {
-//     if(e.type === 'mouseover') {e.target.style.backgroundColor = 'red'}
-//     else if(e.type === 'mouseout') {e.target.style.backgroundColor = 'blue'}
-// }
+const setColorBorder = (e) => {
+    if(e.type === 'mouseenter') {e.target.style.border = '3px solid orange'}
+    else if(e.type === 'mouseleave') {e.target.style.border = '3px solid rgba(255, 255, 255, 0.8)'}
+}
 
-// about.addEventListener('mouseover', setColor)
+about.addEventListener('mouseenter', setColorBorder)
+about.addEventListener('mouseleave', setColorBorder)
 
-about.addEventListener('mouseenter', () => {
-    about.style.border = '3px solid orange'
-    document.querySelector('#name').style.display = 'none'
-    // var name = document.querySelector('#name')
-    // name.style.display = 'none'
-    // name.insertAdjacentHTML('afterbegin', '<div id ="two">HELLO</div>')
+skill.addEventListener('mouseenter', setColorBorder)
+skill.addEventListener('mouseleave', setColorBorder)
+
+contacts.addEventListener('mouseenter', setColorBorder)
+contacts.addEventListener('mouseleave', setColorBorder)
+
+
+// about.addEventListener('mouseenter', () => {
+//     about.style.border = '3px solid orange'
+//     document.querySelector('#name').style.display = 'none'
+//     var name = document.querySelector('#name')
+//     name.style.display = 'none'
+//     name.insertAdjacentHTML('afterbegin', '<div id ="two">HELLO</div>')
     
-})
+// })
 
-skill.addEventListener('mouseenter', () => {
-    skill.style.border = '3px solid orange'
-    document.querySelector('#name').style.display = 'none'
-})
+// skill.addEventListener('mouseenter', () => {
+//     skill.style.border = '3px solid orange'
+//     document.querySelector('#name').style.display = 'none'
+// })
 
-contacts.addEventListener('mouseenter', () => {
-    contacts.style.border = '3px solid orange'
-    document.querySelector('#name').style.display = 'none'
+// contacts.addEventListener('mouseenter', () => {
+//     contacts.style.border = '3px solid orange'
+//     document.querySelector('#name').style.display = 'none'
     
-})
+// })
 
-about.addEventListener('mouseleave', () => {
-    document.querySelector('#name').style.display = ''
-    about.style.border = '3px solid rgba(255, 255, 255, 0.8)'
-})
+// about.addEventListener('mouseleave', () => {
+//     document.querySelector('#name').style.display = ''
+//     about.style.border = '3px solid rgba(255, 255, 255, 0.8)'
+// })
 
-skill.addEventListener('mouseleave', () => {
-    document.querySelector('#name').style.display = ''
-    skill.style.border = '3px solid rgba(255, 255, 255, 0.8)'
-})
+// skill.addEventListener('mouseleave', () => {
+//     document.querySelector('#name').style.display = ''
+//     skill.style.border = '3px solid rgba(255, 255, 255, 0.8)'
+// })
 
-contacts.addEventListener('mouseleave', () => {
-    document.querySelector('#name').style.display = ''
-    contacts.style.border = '3px solid rgba(255, 255, 255, 0.8)'
-})
+// contacts.addEventListener('mouseleave', () => {
+//     document.querySelector('#name').style.display = ''
+//     contacts.style.border = '3px solid rgba(255, 255, 255, 0.8)'
+// })
