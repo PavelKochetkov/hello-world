5b4f3e4b5922fb0eea0c547330e2ecc681b2c8c0
PavelKochetkov <106112045+PavelKochetkov@users.noreply.github.com>
2022-06-11 23:31:47 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,17 +5,19 @@ let contacts = document.querySelector('#contacts')
 let span = document.querySelector('#close')
 let header = document.querySelector('#header')
 
-
 const setColorBorder = (e) => {
     if(e.type === 'mouseenter') {e.target.style.border = '3px solid orange'}
     else if(e.type === 'mouseleave') {e.target.style.border = '3px solid rgba(255, 255, 255, 0.8)'}
 }
 
 const closeModal = (e) => {
-    if(e.type === 'click') {modal.style.display = 'none'}
+    if(e.type === 'click') {
+        modal.style.display = 'none'
+        greetings.style.display = 'block'
+    }
 }
 
-setTimeout(() => {greetings.style.display = 'block'}, 1500)
+setTimeout(() => {greetings.style.display = 'block'}, 1000)
 
 about.addEventListener('mouseenter', setColorBorder)
 about.addEventListener('mouseleave', setColorBorder)
@@ -26,60 +28,18 @@ skill.addEventListener('mouseleave', setColorBorder)
 contacts.addEventListener('mouseenter', setColorBorder)
 contacts.addEventListener('mouseleave', setColorBorder)
 
-about.addEventListener('click', () => {
-    modal.style.display = 'block'
-    header.textContent = about.textContent})
 
 skill.addEventListener('click', () => {
     modal.style.display = 'block'
-    header.textContent = skill.textContent})
-
-contacts.addEventListener('click', () => {
-    modal.style.display = 'block'
-    header.textContent = contacts.textContent})
+    greetings.style.display = 'none'
+    header.textContent = skill.textContent}
+)
 
 span.addEventListener('click', closeModal)
 
 window.onclick = function(event) {
     if (event.target == modal) {
-      modal.style.display = "none"
+      modal.style.display = 'none'
+      greetings.style.display = 'block'
     }
 }
-
-
-
-
-// about.addEventListener('mouseenter', () => {
-//     about.style.border = '3px solid orange'
-//     document.querySelector('#name').style.display = 'none'
-//     var name = document.querySelector('#name')
-//     name.style.display = 'none'
-//     name.insertAdjacentHTML('afterbegin', '<div id ="two">HELLO</div>')
-    
-// })
-
-// skill.addEventListener('mouseenter', () => {
-//     skill.style.border = '3px solid orange'
-//     document.querySelector('#name').style.display = 'none'
-// })
-
-// contacts.addEventListener('mouseenter', () => {
-//     contacts.style.border = '3px solid orange'
-//     document.querySelector('#name').style.display = 'none'
-    
-// })
-
-// about.addEventListener('mouseleave', () => {
-//     document.querySelector('#name').style.display = ''
-//     about.style.border = '3px solid rgba(255, 255, 255, 0.8)'
-// })
-
-// skill.addEventListener('mouseleave', () => {
-//     document.querySelector('#name').style.display = ''
-//     skill.style.border = '3px solid rgba(255, 255, 255, 0.8)'
-// })
-
-// contacts.addEventListener('mouseleave', () => {
-//     document.querySelector('#name').style.display = ''
-//     contacts.style.border = '3px solid rgba(255, 255, 255, 0.8)'
-// })
---
+++ skill.js
@@ -0,0 +1,66 @@
+function Skill() {
+    return(
+    <div id="skillMore">
+        <a href="https://stepik.org/cert/1415848" target="_blank"><img src="img/html5_css3.jpg" id="htmlcss"></img></a>
+        <a href="https://www.udemy.com/certificate/UC-d21429ca-ccf5-4e96-aeb0-fb904466c007/" target="_blank"><img src="img/js.jpg" id="js"></img></a>
+        <img src="img/react.jpg" id="react"></img>
+        <div id="skillComment"><h3>Изучаемые технологии на данный момент</h3></div>
+        <div id="html1" style = {{display:'none'}}><h3>HTML5/CSS3 (Курс пройден)</h3></div>
+        <div id="js1" style = {{display:'none'}}><h3>JavaScript (Курс пройден)</h3></div>
+        <div id="react1" style = {{display:'none'}}><h3>React (В процессе изучения...)</h3></div>     
+    </div>)
+}
+
+ReactDOM.render(
+    <Skill />,
+    document.querySelector('#root')
+    
+)
+
+let js = document.querySelector('#js')
+let react = document.querySelector('#react')
+let htmlcss = document.querySelector('#htmlcss')
+let skillComment = document.querySelector('#skillComment')
+let html1 = document.querySelector('#html1')
+let js1 = document.querySelector('#js1')
+let react1 = document.querySelector('#react1')
+
+js.addEventListener('mouseenter', setColorBorder)
+js.addEventListener('mouseleave', setColorBorder)
+
+react.addEventListener('mouseenter', setColorBorder)
+react.addEventListener('mouseleave', setColorBorder)
+
+htmlcss.addEventListener('mouseenter', setColorBorder)
+htmlcss.addEventListener('mouseleave', setColorBorder)
+
+
+htmlcss.addEventListener('mouseenter', () => {
+    skillComment.style.display = 'none'
+    html1.style.display = 'block'
+})
+
+htmlcss.addEventListener('mouseleave', () => {
+    skillComment.style.display = 'block'
+    html1.style.display = 'none'
+})
+
+js.addEventListener('mouseenter', () => {
+    skillComment.style.display = 'none'
+    js1.style.display = 'block'
+})
+
+js.addEventListener('mouseleave', () => {
+    skillComment.style.display = 'block'
+    js1.style.display = 'none'
+})
+
+react.addEventListener('mouseenter', () => {
+    skillComment.style.display = 'none'
+    react1.style.display = 'block'
+})
+
+react.addEventListener('mouseleave', () => {
+    skillComment.style.display = 'block'
+    react1.style.display = 'none'
+})
